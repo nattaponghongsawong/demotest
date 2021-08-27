c6329fe0f358d86ff2b12f9d7335e4692f74b526
Apitran <62310136@go.buu.ac.th>
2021-08-27 20:01:50 +0700

    folder01

---
+++ myreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Menu from './components/Menu'
+import Header from './components/้Header'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Menu/>
+      <Footer/>
+      <Content/>
+    </div>
+  )
+}
